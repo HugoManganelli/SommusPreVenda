b494726334651cc20e527c965f1e4b06b4d5bb31
HugoManganelli <hugoteixeira@sommus.com>
2017-04-13 18:51:41 -0300

    Concluímos o PreVendaController

---
+++ Presentation/SommusPreVenda.WebService/Controllers/PreVendaController.cs
@@ -0,0 +1,24 @@
+﻿using SommusPreVenda.Application.Application;
+using SommusPreVenda.Application.ViewModels.PreVenda;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SommusPreVenda.WebService.Controllers
+{
+    public class PreVendaController : ApiController
+    {
+        public HttpResponseMessage Post(PreVendaVM preVendaVM)
+        {
+            PreVendaApplication.Add(preVendaVM);
+            if (PreVendaApplication.ResponseType.Equals("Error"))
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, PreVendaApplication.ResponseMessage);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.Created, PreVendaApplication.ResponseMessage);
+            }
+        }
+    }
+}
